7c115a107e52232ad747f0fb0213e91090f038ba
blueve-xi <gregorymcdaniel1995@protonmaiil.com>
2025-09-06 21:01:55 -0500

    edit notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,5 +2,11 @@ vim.pack.add({
 		{ src = "https://github.com/rcarriga/nvim-notify.git" },
 })
 
-vim.notify = require("notify")
+require("notify").setup({
+		timeout = 5000,
+})
+
+ vim.notify = require("notify")
 vim.opt.termguicolors = true
+
+
