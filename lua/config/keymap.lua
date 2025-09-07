5fdc297c3e96f36c72917cc0f65c5fc34adbf920
blueve-xi <gregorymcdaniel1995@protonmaiil.com>
2025-09-07 09:52:07 -0500

    lua changes

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,12 +2,11 @@
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 
 --Plugin Keymaps
-vim.keymap.set('n', '<leader>nt', ':Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>ee', ':Neotree toggle<CR>')
 vim.keymap.set('n', '<C-f>', ':Neotree focus<CR>')
 vim.keymap.set('n', '<leader>lg', '<cmd>LazyGit<CR>')
-vim.keymap.set('n', '<leader>tt', ':Themify<CR>')
+vim.keymap.set('n', '<leader>t', ':Themify<CR>')
 vim.keymap.set('n', '<C-t>', ':Twilight<CR>')
 vim.keymap.set('n', '<leader>nd', require("notify").dismiss)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,8 @@
--- system setup and keymaps
+-- system setup
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = false
 vim.opt.tabstop = 4
 vim.opt.swapfile = false
-vim.g.mapleader = " "
-
