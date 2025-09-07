4048ecd44c0a3f066626e4bcc87e398b2ea5ff91
blueve-xi <gregorymcdaniel1995@protonmaiil.com>
2025-09-07 07:55:38 -0500

    updated lua file

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
--- system setup and keymaps
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.wrap = false
-vim.o.tabstop = 4
-vim.o.swapfile = false
-vim.g.mapleader = " "
-
 -- (plugins)
 require('plugins.lspconfig')
 require('plugins.neo-tree')
@@ -23,4 +15,4 @@ require('plugins.notify')
 -- require configs
 require('config.dashboard-nvim')
 require('config.keybinds')
-
+require('config.options')
